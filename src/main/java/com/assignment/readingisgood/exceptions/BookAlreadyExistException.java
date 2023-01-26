d33200f6ad05cc520d183a8f952d6e674e35c419
Piyush Gupta <34481252+piyush-gupta1999@users.noreply.github.com>
2023-01-26 18:13:01 +0530

    Added  Sonarqube For Code quality Improvement (#9)
    * Added sonarqube for code quality
    
    * Added jacoco plugin for code coverage.
    
    * Code smell improvements suggested by sonarqube

--- src/main/java/com/assignment/readingisgood/exceptions/BookAlreadyExistException.java
+++ src/main/java/com/assignment/readingisgood/exceptions/BookAlreadyExistException.java
@@ -1,6 +1,6 @@
 package com.assignment.readingisgood.exceptions;
 
-public class BookAlreadyExistException extends Exception{
+public class BookAlreadyExistException extends Exception {
     public BookAlreadyExistException(String message) {
         super(message);
     }
--- src/main/java/com/assignment/readingisgood/exceptions/OutOfStockException.java
+++ src/main/java/com/assignment/readingisgood/exceptions/OutOfStockException.java
@@ -1,6 +1,6 @@
 package com.assignment.readingisgood.exceptions;
 
-public class OutOfStockException extends Throwable {
+public class OutOfStockException extends Exception {
     public OutOfStockException(String message) {
         super(message);
     }
